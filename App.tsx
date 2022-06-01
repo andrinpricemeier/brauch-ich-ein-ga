bb132f0a48da9bb89c2cf9a43ac71004e1a9e1ba
Andrin Meier <andrin@pricemeier.ch>
2022-06-01 07:47:06 +0200

    Remove storybook because its support for react native seems to be somewhat lacking

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
+import { View } from "react-native";
 import { TailwindProvider } from "tailwind-rn";
-import Hello from "./src/Hello";
 import utilities from "./tailwind.json";
 
 export default function App() {
   return (
     <TailwindProvider utilities={utilities}>
-      <Hello />
+      <View />
     </TailwindProvider>
   );
 }
