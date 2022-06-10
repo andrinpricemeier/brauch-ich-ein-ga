beb692d3c57c6ec2246f7d817eae559e00ea4a6f
Andrin Meier <andrin@pricemeier.ch>
2022-06-10 17:23:23 +0200

    Setup vitest for unit testing

--- src/model/ShouldIBuyAGASpecification.ts
+++ src/model/ShouldIBuyAGASpecification.ts
@@ -1,4 +1,5 @@
-import { Dinero, lessThan } from "dinero.js";
+import { dinero, Dinero, lessThan } from "dinero.js";
+import { CHF } from "@dinero.js/currencies";
 
 export class ShouldIBuyAGASpecification {
   isSatisifiedBy(
@@ -7,4 +8,32 @@ export class ShouldIBuyAGASpecification {
   ) {
     return lessThan(monthlyGAAverage, monthlyTicketAverage);
   }
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+  it("given a lower monthly GA average than ticket average, should tell user to buy a GA", () => {
+    expect(
+      new ShouldIBuyAGASpecification().isSatisifiedBy(
+        dinero({ amount: 200, currency: CHF }),
+        dinero({ amount: 300, currency: CHF })
+      )
+    ).toBeTruthy();
+  });
+  it("given a higher monthly GA average than ticket average, should tell user NOT to buy a GA", () => {
+    expect(
+      new ShouldIBuyAGASpecification().isSatisifiedBy(
+        dinero({ amount: 300, currency: CHF }),
+        dinero({ amount: 200, currency: CHF })
+      )
+    ).toBeFalsy();
+  });
+  it("given the same monthly GA average as ticket average, should tell user NOT to buy a GA", () => {
+    expect(
+      new ShouldIBuyAGASpecification().isSatisifiedBy(
+        dinero({ amount: 300, currency: CHF }),
+        dinero({ amount: 300, currency: CHF })
+      )
+    ).toBeFalsy();
+  });
 }
