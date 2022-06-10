c55fbb219a83e44076c7b5735e4ed26faedd2f30
Andrin Meier <andrin@pricemeier.ch>
2022-06-11 01:56:35 +0200

    Add unit tests to CI pipeline and sonar cloud

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {},
+  test: {
+    coverage: {
+      reportsDirectory: "coverage/unit",
+      reporter: ["text", "lcov"],
+    },
+  },
 });
