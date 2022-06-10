2500a31c2e56a2580b7e2cee18fef4075e6a51ee
Andrin Meier <andrin@pricemeier.ch>
2022-06-10 17:32:45 +0200

    Remove inline-source tests since cypress would require additional configuration for it to work

--- src/model/ShouldIBuyAGASpecification.ts
+++ src/model/ShouldIBuyAGASpecification.ts
@@ -9,31 +9,3 @@ export class ShouldIBuyAGASpecification {
     return lessThan(monthlyGAAverage, monthlyTicketAverage);
   }
 }
-
-if (import.meta.vitest) {
-  const { it, expect } = import.meta.vitest;
-  it("given a lower monthly GA average than ticket average, should tell user to buy a GA", () => {
-    expect(
-      new ShouldIBuyAGASpecification().isSatisifiedBy(
-        dinero({ amount: 200, currency: CHF }),
-        dinero({ amount: 300, currency: CHF })
-      )
-    ).toBeTruthy();
-  });
-  it("given a higher monthly GA average than ticket average, should tell user NOT to buy a GA", () => {
-    expect(
-      new ShouldIBuyAGASpecification().isSatisifiedBy(
-        dinero({ amount: 300, currency: CHF }),
-        dinero({ amount: 200, currency: CHF })
-      )
-    ).toBeFalsy();
-  });
-  it("given the same monthly GA average as ticket average, should tell user NOT to buy a GA", () => {
-    expect(
-      new ShouldIBuyAGASpecification().isSatisifiedBy(
-        dinero({ amount: 300, currency: CHF }),
-        dinero({ amount: 300, currency: CHF })
-      )
-    ).toBeFalsy();
-  });
-}
