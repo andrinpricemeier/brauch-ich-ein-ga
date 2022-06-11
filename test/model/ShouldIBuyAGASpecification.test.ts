6cfe6a739f0d806af4420793a0188b2fb1047bbe
Andrin Meier <andrin@pricemeier.ch>
2022-06-11 02:44:08 +0200

    Replace tests with property tests

--- test/model/ShouldIBuyAGASpecification.test.ts
+++ test/model/ShouldIBuyAGASpecification.test.ts
@@ -2,30 +2,37 @@ import { expect, describe, it } from "vitest";
 import { ShouldIBuyAGASpecification } from "../../src/model/ShouldIBuyAGASpecification";
 import { dinero } from "dinero.js";
 import { CHF } from "@dinero.js/currencies";
+import * as fc from "fast-check";
 
 describe("Given the 'Should I buy a GA' specification", () => {
-  it("When the GA average is higher than the ticket average, should signal user to NOT buy a GA", () => {
-    expect(
-      new ShouldIBuyAGASpecification().isSatisifiedBy(
-        dinero({ amount: 300, currency: CHF }),
-        dinero({ amount: 200, currency: CHF })
-      )
-    ).toBeFalsy();
-  });
   it("When the GA average is lower than the ticket average, should signal user to buy a GA", () => {
-    expect(
-      new ShouldIBuyAGASpecification().isSatisifiedBy(
-        dinero({ amount: 200, currency: CHF }),
-        dinero({ amount: 300, currency: CHF })
-      )
-    ).toBeTruthy();
+    const minMax = fc.tuple(fc.nat(), fc.nat()).filter((t) => t[0] < t[1]);
+    fc.assert(
+      fc.property(minMax, (a: number[]) => {
+        const gaAverage = a[0];
+        const ticketAverage = a[1];
+        expect(
+          new ShouldIBuyAGASpecification().isSatisifiedBy(
+            dinero({ amount: gaAverage, currency: CHF }),
+            dinero({ amount: ticketAverage, currency: CHF })
+          )
+        ).toBeTruthy();
+      })
+    );
   });
-  it("When the GA average is equal to the ticket average, should signal user to NOT buy a GA", () => {
-    expect(
-      new ShouldIBuyAGASpecification().isSatisifiedBy(
-        dinero({ amount: 300, currency: CHF }),
-        dinero({ amount: 300, currency: CHF })
-      )
-    ).toBeFalsy();
+  it("When the GA average is higher or equal to the ticket average, should signal user to NOT buy a GA", () => {
+    const minMax = fc.tuple(fc.nat(), fc.nat()).filter((t) => t[0] >= t[1]);
+    fc.assert(
+      fc.property(minMax, (a: number[]) => {
+        const gaAverage = a[0];
+        const ticketAverage = a[1];
+        expect(
+          new ShouldIBuyAGASpecification().isSatisifiedBy(
+            dinero({ amount: gaAverage, currency: CHF }),
+            dinero({ amount: ticketAverage, currency: CHF })
+          )
+        ).toBeFalsy();
+      })
+    );
   });
 });
